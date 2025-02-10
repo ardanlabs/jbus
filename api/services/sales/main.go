453bc7fe9b0866012f83e63959905dff877a46aa
William Kennedy <bill@ardanlabs.com>
2025-02-10 10:38:36 -0500

    commit 3

--- api/services/sales/main.go
+++ api/services/sales/main.go
@@ -1,4 +1,51 @@
 package main
 
+import (
+	"context"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/ardanlabs/service/foundation/logger"
+)
+
 func main() {
+	var log *logger.Logger
+
+	traceIDFn := func(ctx context.Context) string {
+		return ""
+	}
+
+	log = logger.New(os.Stdout, logger.LevelInfo, "SALES", traceIDFn)
+
+	// -------------------------------------------------------------------------
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Error(ctx, "startup", "err", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, log *logger.Logger) error {
+
+	// -------------------------------------------------------------------------
+	// GOMAXPROCS
+
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// -------------------------------------------------------------------------
+	// Shutdown
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-shutdown
+
+	log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
+
+	return nil
 }
