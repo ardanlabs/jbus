30d7e24bfe0a6fc81f3a3d9376bf408c4637fdd4
William Kennedy <bill@ardanlabs.com>
2025-02-12 09:25:42 -0500

    commit 19

--- app/sdk/mux/mux.go
+++ app/sdk/mux/mux.go
@@ -2,11 +2,13 @@ package mux
 
 import (
 	"github.com/ardanlabs/service/app/domain/testapp"
+	"github.com/ardanlabs/service/app/sdk/mid"
+	"github.com/ardanlabs/service/foundation/logger"
 	"github.com/ardanlabs/service/foundation/web"
 )
 
-func WebAPI() *web.App {
-	app := web.NewApp()
+func WebAPI(log *logger.Logger) *web.App {
+	app := web.NewApp(mid.Logger(log))
 
 	testapp.Routes(app)
 
